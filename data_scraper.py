e54d5b9b8f93d52616b08635d4eec8605c078020
SirjanK <sxkafle12@gmail.com>
2024-12-18 09:50:01 -0800

    amends to the scraper

--- data_scraper.py
+++ data_scraper.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+from typing import Optional
 
 
 URL = "https://www.transtats.bts.gov/DL_SelectFields.aspx?gnoyr_VQ=FGK&QO_fu146_anzr=b0-gvzr"
@@ -17,7 +18,6 @@ DOWNLOAD_DIR = os.path.expanduser("~/Downloads")
 
 # List of IDs for checkboxes we want to include in our download
 OPTIONS = [
-    "DAY_OF_WEEK",
     "FL_DATE",
     "OP_CARRIER_AIRLINE_ID",
     "ORIGIN_AIRPORT_ID",
@@ -28,8 +28,18 @@ OPTIONS = [
     "DIVERTED",
 ]
 
-YEARS = [2018, 2019, 2020, 2021, 2022, 2023, 2024]
-MONTHS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+# YEAR to MONTH config - this allows us to be flexible for which months to download for year.
+# Helps when we have to partially download data for a year.
+YEAR_TO_MONTH_CONFIG = {
+    # 2018: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    2018: [9, 10, 11, 12],
+    2019: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    2020: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    2021: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    2022: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    2023: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    2024: [1, 2, 3, 4, 5, 6, 7, 8],  # only data up to Aug 2024
+}
 
 
 def get_files(download_dir: str) -> set:
@@ -37,8 +47,14 @@ def get_files(download_dir: str) -> set:
     return set(os.listdir(download_dir))
 
 
-def wait_for_download(download_dir: str, timeout: int = 300):
-    """Wait for download to complete."""
+def wait_for_download(download_dir: str, timeout: int = 300) -> Optional[str]:
+    """
+    Wait for download to complete.
+
+    :param download_dir: Directory where the file is downloaded.
+    :param timeout: Timeout in seconds.
+    :return filename of the downloaded file if successful, None otherwise.
+    """
     # initial set of files
     initial_files = get_files(download_dir)
     seconds = 0
@@ -46,9 +62,23 @@ def wait_for_download(download_dir: str, timeout: int = 300):
         time.sleep(5)
         for fname in get_files(download_dir) - initial_files:
             if fname.endswith(".csv"):  # we successfully downloaded a file
-                return True
+                return fname
         seconds += 5
-    return False
+
+
+def replace_file(old_file: str, download_dir: str, year: int, month: int) -> None:
+    """
+    Replace the old_file with a renamed version based on year and month.
+
+    :param old_file: Old file name.
+    :param download_dir: Directory where the file is downloaded.
+    :param year: Year of the file.
+    :param month: Month of the file.
+    """
+    old_fpath = os.path.join(download_dir, old_file)
+    new_fpath = os.path.join(download_dir, f"T_ONTIME_MARKETING_{year}_{month}.csv")
+    if os.path.exists(old_file):
+        os.remove(old_file)
 
 
 def scrape_data() -> None:
@@ -62,7 +92,7 @@ def scrape_data() -> None:
     try:
         # Applicable for all years and months
         # Navigate to the specified URL
-        driver.get("https://www.transtats.bts.gov/DL_SelectFields.aspx?gnoyr_VQ=FGK&QO_fu146_anzr=b0-gvzr")
+        driver.get(URL)
         # Wait for the page to load and elements to be present
         wait = WebDriverWait(driver, 10)
 
@@ -76,12 +106,8 @@ def scrape_data() -> None:
             if not checkbox.is_selected():
                 checkbox.click()
 
-        for year in YEARS:
-            for month in MONTHS:
-                if year == 2024 and month >= 12:
-                    # We only have data up to November 2024 as of now
-                    continue 
-
+        for year in YEAR_TO_MONTH_CONFIG:
+            for month in YEAR_TO_MONTH_CONFIG[year]:
                 year_dropdown = wait.until(EC.element_to_be_clickable((By.ID, "cboYear")))
                 year_dropdown.send_keys(str(year))
 
@@ -93,10 +119,15 @@ def scrape_data() -> None:
                 download_button.click()
 
                 # Wait for the download to complete
-                if wait_for_download(DOWNLOAD_DIR):
-                    print(f"DOWNLOADED FILE FOR YEAR {year} AND MONTH {month}")
-                else:
+                tmp_file = wait_for_download(DOWNLOAD_DIR)
+                if tmp_file is None:
                     print(f"DOWNLOAD FAILED FOR YEAR {year} AND MONTH {month}")
+                else:
+                    replace_file(tmp_file, DOWNLOAD_DIR, year, month)
+                    print(f"DOWNLOADED FILE FOR YEAR {year} AND MONTH {month}")
+
+                # bit of buffer time to not quickly re-click Download
+                time.sleep(2)
     finally:
         # Close the driver
         driver.quit()
