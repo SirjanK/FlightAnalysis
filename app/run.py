5989285d5acf11aef8dcb05845a874b190e1720b
SirjanK <sxkafle12@gmail.com>
2024-12-19 10:36:45 -0800

    add react project

--- app/run.py
+++ app/run.py
@@ -5,7 +5,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 # Add the parent directory to sys.path
 sys.path.append(parent_dir)
-from app.routes import app
+from app.backend.routes import app
 
 
 if __name__ == '__main__':
