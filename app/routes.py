971efde2f4c10aff78ee38cb56d1799ba24f1860
SirjanK <sxkafle12@gmail.com>
2024-12-18 22:50:01 -0800

    hook up flask code with delay calculator

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, Flask, request, jsonify
+from flask import render_template, Flask, request, jsonify, abort
 import sys
 import os
+import pandas as pd
 # Get the parent directory
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Add the parent directory to sys.path
@@ -8,7 +9,28 @@ sys.path.append(parent_dir)
 from flight.delay_calculator import DelayCalculator
 
 app = Flask(__name__)
-delay_calculator = DelayCalculator()
+
+
+ASSETS_DIR = "assets/"
+
+
+def init_delay_calculator() -> DelayCalculator:
+    """
+    Initialize the delay calculator
+    """
+
+    print("Initializing delay calculator...")
+    params_df = pd.read_csv(f"{ASSETS_DIR}/model_params.csv")
+    return DelayCalculator(params_df=params_df)
+
+
+delay_calculator = init_delay_calculator()
+airline_lookup_df = pd.read_csv(f"{ASSETS_DIR}/airline_id_lookup.csv")
+airport_lookup_df = pd.read_csv(f"{ASSETS_DIR}/airport_id_lookup.csv")
+# create dictionary from description to code
+airline_lookup = airline_lookup_df.set_index('Description')['Code'].to_dict()
+airport_lookup = airport_lookup_df.set_index('Description')['Code'].to_dict()
+
 
 @app.route('/')
 def index():
@@ -22,10 +44,26 @@ def predict():
     destination = request.form.get('destination')
     airline = request.form.get('airline')
     departure_time = request.form.get('departure_time')
+
+    if origin is not None and origin not in airport_lookup_df:
+        abort(400, description="Unsupported input: origin")
+    if destination is not None and destination not in airport_lookup_df:
+        abort(400, description="Unsupported input: destination")
+    if airline is not None and airline not in airline_lookup_df:
+        abort(400, description="Unsupported input: airline")
+    if departure_time is not None and not departure_time in ['morning', 'afternoon', 'evening', 'night']:
+        abort(400, description="Unsupported input: departure_time")
     
     # Predict the delays
-    # TODO implement name to ID mapping
-    delays = delay_calculator.predict_delays(None, None, None, None)
+    delays = delay_calculator.predict_delays(
+        orig_airport_id=airport_lookup[origin],
+        dest_airport_id=airport_lookup[destination],
+        airline_id=airline_lookup[airline],
+        departure_time=departure_time,
+    )
+
+    if delays is None:
+        abort(422, description="Delay calculator cannot support this query")
     
     # Convert numpy array to list for JSON serialization
     delays_list = delays.tolist()
@@ -33,3 +71,13 @@ def predict():
     return jsonify({
         'delay_data': delays_list
     })
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify(error=str(error.description)), 400
+
+
+@app.errorhandler(422)
+def unprocessable_entity(error):
+    return jsonify(error=str(error.description)), 422
--- flight/delay_calculator.py
+++ flight/delay_calculator.py
@@ -16,7 +16,7 @@ class DelayCalculator:
 
         self._params_df = params_df.index(CONDITIONAL_COLS)
 
-    def predict_delays(self, orig_airport_id: Optional[int], dest_airport_id: Optional[int], airline_id: Optional[int], time_bucket: Optional[str]) -> np.ndarray:
+    def predict_delays(self, orig_airport_id: Optional[int], dest_airport_id: Optional[int], airline_id: Optional[int], time_bucket: Optional[str]) -> Optional[np.ndarray]:
         """
         Predict delay probabilities given configurations.
 
@@ -26,10 +26,14 @@ class DelayCalculator:
         :param hour_of_day: Hour of the day.
         :return: predicted probabilities of delays >= T. The array is of shape (T, 2) where T is the number of thresholds (i.e. delay durations)
                  arr[:, 0] is the delay duration, arr[:, 1] is the delay probability (1 - CDF).
+                 if the query is not supported, return None.
         """
 
         # get parameters
-        fitted_p, fitted_lambda = self._params_df.loc[(orig_airport_id, dest_airport_id, airline_id, time_bucket), ['p', 'lambda']]
+        fitted_p, fitted_lambda = self._params_df.loc[(orig_airport_id, dest_airport_id, airline_id, time_bucket), ['p', 'lambda']].get([0, 1], [None, None])
+
+        if fitted_p is None or fitted_lambda is None:
+            return None
 
         # 1 - CDF for delta > 0
         durations = np.linspace(start=0, stop=181, num=1801)
